97c2704913f92330ee9ef313ebb48574aa8a6431
Hamish Hall <hachall@hotmail.com>
2019-06-21 12:02:25 +0100

    col row changes for responsiveness on safari

--- src/containers/cameras/cameras.jsx
+++ src/containers/cameras/cameras.jsx
@@ -8,17 +8,17 @@ class Cameras extends Component {
     return (
       <div className="camera-section">
 
-        <div className="row">
-          <div className="col-xs-12 col-md-6 camera-split">
+        <div className="">
+          <div className="col-12 col-sm-12 col-md-6 camera-split">
             <CameraBox name={"Camera 1"} id={"Camera1"}/>
           </div>
-          <div className="col-xs-12 col-md-6 camera-split">
+          <div className="col-12 col-sm-12 col-md-6 camera-split">
             <CameraBox name={"Camera 2"} id={"Camera2"}/>
           </div>
-          <div className="col-xs-12 col-md-6 camera-split">
+          <div className="col-12 col-sm-12 col-md-6 camera-split">
             <CameraBox name={"Camera 3"} id={"Camera3"}/>
           </div>
-          <div className="col-xs-12 col-md-6 camera-split">
+          <div className="col-12 col-sm-12 col-md-6 camera-split">
             <CameraBox name={"Camera 4"} id={"Camera4"}/>
           </div>
         </div>
