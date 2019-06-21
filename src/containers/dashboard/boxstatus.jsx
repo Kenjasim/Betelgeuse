c2c9bd35d89fc540cf4d33c22c8aa6a6abbe4586
Kenan <kenanabid10@gmail.com>
2019-06-21 12:31:14 +0100

    tried another fix

--- src/containers/dashboard/boxstatus.jsx
+++ src/containers/dashboard/boxstatus.jsx
@@ -36,7 +36,7 @@ class BoxStatus extends Component {
       percentpower: '',
       status: "Connected",
       response: false,
-      power_endpoint: 'https://217.138.134.182:3001'
+      power_endpoint: 'http://217.138.134.182:3001'
     }
     this.socket = io.connect(this.state.power_endpoint)
     this.donut = this.donut.bind(this)
@@ -72,8 +72,8 @@ class BoxStatus extends Component {
   }
 
   donut = () => {
-    const url = "https://217.138.134.182:3333/?psqlQuery="
-    const temp_url = "https://10.0.0.43:3333/?psqlQuery="
+    const url = "http://217.138.134.182:3333/?psqlQuery="
+    const temp_url = "http://10.0.0.43:3333/?psqlQuery="
     let d = new Date(new Date().getFullYear(),new Date().getMonth() , new Date().getDate())
     let cd = new Date();
     const query = `SELECT AVG("Current") FROM "Power" WHERE "TimeLocal" > '${this.convertDate(d)}' AND "TimeLocal" < '${this.convertDate(cd.setDate(d.getDate() + 1))}' AND "Current" < 6 AND "Current" > 0`
--- src/containers/dashboard/systems.jsx
+++ src/containers/dashboard/systems.jsx
@@ -6,8 +6,8 @@ import RadarImagesStatus from './radar_images_status'
 import CameraStatus from './camera_status'
 
 const systems = [
-  <SystemBox name="AIS" endpoint='https://217.138.134.182:3002' link='/ais' />,
-  <SystemBox name="GPS" endpoint='https://217.138.134.182:3003' link='/' />,
+  <SystemBox name="AIS" endpoint='http://217.138.134.182:3002' link='/ais' />,
+  <SystemBox name="GPS" endpoint='http://217.138.134.182:3003' link='/' />,
   <SystemBox name="Radar Data" link='/radar' disabled={true}/>,
   <RadarImagesStatus name="Radar Images" link='/radar'/>,
   <SystemBox name="WiFi Pinger" link='/wifi_pinger' disabled={true}/>,
@@ -16,9 +16,9 @@ const systems = [
   // <CameraStatus name="Camera 2" link='/cameras'/>,
   // <CameraStatus name="Camera 3" link='/cameras'/>,
   // <CameraStatus name="Camera 4" link='/cameras' />,
-  <SystemBox name="Power" endpoint='https://217.138.134.182:3001' link='/power' />,
-  <SystemBox name="MRU" endpoint='https://217.138.134.182:3004' link='/mru' />,
-  <SystemBox name="Weather" endpoint='https://217.138.134.182:3000'link='/weather' />
+  <SystemBox name="Power" endpoint='http://217.138.134.182:3001' link='/power' />,
+  <SystemBox name="MRU" endpoint='http://217.138.134.182:3004' link='/mru' />,
+  <SystemBox name="Weather" endpoint='http://217.138.134.182:3000'link='/weather' />
 ]
 
 class Systems extends Component {
@@ -32,7 +32,7 @@ class Systems extends Component {
           Systems:
         </div>
         <div className="system-boxes-container">
-          <div className="">
+          <div className="row">
             {systems.map((system) => {
               return <div className="col-xs-6 systems-columns" key={system.name}>{system}</div>
             })}
