a87e8c2d5213c19449fdc31ade794ea2d53ef945
Engineering <pestochicken420@gmail.com>
2019-07-01 19:29:41 +0100

    tried to use reactplayer for rtsp live stream with cameras but need a workaround

--- src/containers/cameras/camera_feed.jsx
+++ src/containers/cameras/camera_feed.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import ReactPlayer from 'react-player'
 
 class CameraFeed extends Component {
   render () {
     return (
-      <div className="camera-feed"></div>
+      <div className="camera-feed">
+        <ReactPlayer
+                className='react-player '
+                url={"rtsp://admin:yeetyeet@camelabobeyera.ddns.net:554//h264Preview_01_main"}
+                height="80%"
+                width="100%"
+                controls = "true"
+              />
+      </div>
     )
   }
 }
