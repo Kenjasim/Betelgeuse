9dd3447d5219b6264b5d4b32e5aa2ef38e094329
Hamish Hall <hachall@hotmail.com>
2019-07-10 16:00:57 +0100

    box status in redux state, updates display on dashboard

--- src/containers/dashboard/systems.jsx
+++ src/containers/dashboard/systems.jsx
@@ -6,19 +6,19 @@ import RadarImagesStatus from './radar_images_status'
 import CameraStatus from './camera_status'
 
 const systems = [
-  <SystemBox name="AIS" endpoint='bobeyes.siriusinsight.io:3002' link='/ais' />,
-  <SystemBox name="GPS" endpoint='bobeyes.siriusinsight.io:3003' link='/' />,
-  <SystemBox name="Radar Data" link='/radar' disabled={true}/>,
-  <RadarImagesStatus name="Radar Images" link='/radar'/>,
-  <SystemBox name="WiFi Pinger" link='/wifi_pinger' disabled={true}/>,
-  <SystemBox name="Direction Finder" link='direction_finder' disabled={true}/>,
+  <SystemBox name="AIS" key="AIS" endpoint='bobeyes.siriusinsight.io:3002' link='/ais' />,
+  <SystemBox name="GPS" key="GPS" endpoint='bobeyes.siriusinsight.io:3003' link='/' />,
+  <SystemBox name="Radar Data" key="Radar Data" link='/radar' disabled={true}/>,
+  <RadarImagesStatus name="Radar Images" key="Radar Images" link='/radar'/>,
+  <SystemBox name="WiFi Pinger" key="WiFi Pinger" link='/wifi_pinger' disabled={true}/>,
+  <SystemBox name="Direction Finder" key="Direction Finder" link='direction_finder' disabled={true}/>,
   // <CameraStatus name="Camera 1" link='/cameras'/>,
   // <CameraStatus name="Camera 2" link='/cameras'/>,
   // <CameraStatus name="Camera 3" link='/cameras'/>,
   // <CameraStatus name="Camera 4" link='/cameras' />,
-  <SystemBox name="Power" endpoint='bobeyes.siriusinsight.io:3001' link='/power' />,
-  <SystemBox name="MRU" endpoint='bobeyes.siriusinsight.io:3004' link='/mru' />,
-  <SystemBox name="Weather" endpoint='bobeyes.siriusinsight.io:3000'link='/weather' />
+  <SystemBox name="Power" key="Power" endpoint='bobeyes.siriusinsight.io:3001' link='/power' />,
+  <SystemBox name="MRU" key="MRU" endpoint='bobeyes.siriusinsight.io:3004' link='/mru' />,
+  <SystemBox name="Weather" key="Weather" endpoint='bobeyes.siriusinsight.io:3000'link='/weather' />
 ];
 
 class Systems extends Component {
