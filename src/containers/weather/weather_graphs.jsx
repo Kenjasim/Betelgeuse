42e8d4813df46246e04055793db43d1d52069daf
Hamish Hall <hachall@hotmail.com>
2019-07-16 15:32:42 +0100

    queryable graph for weather columns

--- src/containers/weather/weather_graphs.jsx
+++ src/containers/weather/weather_graphs.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
 import { Line } from 'react-chartjs-2';
+import moment from 'moment';
 
 class WeatherGraphs extends Component {
   constructor(props) {
@@ -28,7 +29,7 @@ class WeatherGraphs extends Component {
 
   handleChange = (date) => {
     this.setState({ startDate: date }, () => {
-      // this.fetchData()
+      this.fetchData()
 
       console.log("weather graph date changed")
     });
@@ -50,7 +51,62 @@ class WeatherGraphs extends Component {
   }
 
   switchDataTab = (e) => {
-    this.setState({current_tab: e.target.innerHTML})
+    this.setState({current_tab: e.target.innerHTML}, () => {
+      this.fetchData()
+
+    })
+  }
+
+  fetchData = () => {
+    const url = "http://bobeyes.siriusinsight.io:3333/?psqlQuery="
+    const query = `SELECT "TimeLocal", "${this.state.current_tab}" FROM "Weather" WHERE "TimeLocal" BETWEEN '${this.convertDate(this.state.startDate).slice(0,10)} 00:00:01' AND '${this.convertDate(this.state.startDate).slice(0,10)} 23:59:59' AND "ID" %2530 = 0 ORDER BY "TimeLocal" desc`
+    console.log(query)
+    const request = fetch(url+query)
+      .then(response=> response.json())
+      .then((data) => {
+        const n = data.length;
+        const labels = Array(n).join(".").split(".")
+
+
+        const outputData = data.map( Object.values );
+        let chart_data = outputData.map(a => a[1]);
+        let chart_times = outputData.map(a => a[0])
+
+        let color1 = '#FFCC00'
+        let color2 = '#FFF5CC'
+
+        switch (this.state.current_tab) {
+          case 'Temperature':
+            console.log(this.state.current_tab)
+            break
+          case 'WindSpeed':
+            color1 = '#5198F4'
+            color2 = '#E7F1FC'
+        }
+
+
+        this.setState({
+          graph_data: {
+            labels: chart_times,
+            datasets: [
+              {
+                label: this.state.current_tab,
+                data: chart_data,
+                fill: true,
+                backgroundColor: color2,
+                borderColor: color1,
+                pointBackgroundColor: color1,
+                pointBorderColor: color1,
+                pointRadius: 1.5
+              }
+            ],
+          }
+        })
+      })
+  }
+
+  componentDidMount() {
+    this.fetchData()
   }
 
   render() {
@@ -63,7 +119,7 @@ class WeatherGraphs extends Component {
           />
           <div className="weather-graph-options">
             {
-              ["Temperature", "Wind Speed", "Humidity", "Pressure"].map((tab) => {
+              ["Temperature", "WindSpeed", "Humidity", "Pressure"].map((tab) => {
                 let classes = "weather-option-btn2"
                 if (this.state.current_tab == tab) {
                   classes += " weather-selected"
@@ -77,13 +133,27 @@ class WeatherGraphs extends Component {
         </div>
         <div className="weather-graph-wrapper">
           <Line
-              data={this.state.wind_data}
-              height={"100%"}
-              width={"100%"}
+              data={this.state.graph_data}
+
               options={{
                 maintainAspectRatio: false,
-                axes: {
-                  display: false
+                lineTension: 0,
+                scales: {
+                  xAxes: [{
+                    gridLines: {
+                      display:false
+                    },
+                        type: 'time',
+                    ticks: {
+                        autoSkip: true,
+                        maxTicksLimit: 20
+                    }
+                  }],
+                  yAxes: [{
+                      gridLines: {
+                          display:false
+                      }
+                  }]
                 }
               }}
           />
