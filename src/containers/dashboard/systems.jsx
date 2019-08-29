67c3dfeacb1362764120b5310ef05f12b3e8ccdb
Engineering Team <pestochicken420@gmail.com>
2019-08-29 12:03:56 +0100

    yeet 2

--- src/containers/dashboard/systems.jsx
+++ src/containers/dashboard/systems.jsx
@@ -7,20 +7,20 @@ import CameraStatus from './camera_status'
 
 const systems = [
 
-  // <SystemBox name="AIS" key="AIS" endpoint='bobeyes.siriusinsight.io:3112' link='/ais' />,
-  // <SystemBox name="GPS" key="GPS" endpoint='bobeyes.siriusinsight.io:3003' link='/' />,
-  // <SystemBox name="Radar Data" key="Radar Data" link='/radar' disabled={true}/>,
-  // <RadarImagesStatus name="Radar Images" key="Radar Images" link='/radar'/>,
-  // <SystemBox name="WiFi Pinger" key="WiFi Pinger" endpoint='bobeyes.siriusinsight.io:3007' link='/wifi_pinger' />,
-  // <SystemBox name="Direction Finder" key="Direction Finder" link='direction_finder' disabled={true}/>,
+  <SystemBox name="AIS" key="AIS" endpoint='bobeyes.siriusinsight.io:3112' link='/ais' />,
+  <SystemBox name="GPS" key="GPS" endpoint='bobeyes.siriusinsight.io:3003' link='/' />,
+  <SystemBox name="Radar Data" key="Radar Data" link='/radar' disabled={true}/>,
+  <RadarImagesStatus name="Radar Images" key="Radar Images" link='/radar'/>,
+  <SystemBox name="WiFi Pinger" key="WiFi Pinger" endpoint='bobeyes.siriusinsight.io:3007' link='/wifi_pinger' />,
+  <SystemBox name="Direction Finder" key="Direction Finder" link='direction_finder' disabled={true}/>,
 
-  // // <CameraStatus name="Camera 1" link='/cameras'/>,
-  // // <CameraStatus name="Camera 2" link='/cameras'/>,
-  // // <CameraStatus name="Camera 3" link='/cameras'/>,
-  // // <CameraStatus name="Camera 4" link='/cameras' />,
-  // <SystemBox name="Power" key="Power" endpoint='bobeyes.siriusinsight.io:3111' link='/power' />,
-  // <SystemBox name="MRU" key="MRU" endpoint='bobeyes.siriusinsight.io:3004' link='/mru' />,
-  // <SystemBox name="Weather" key="Weather" endpoint='bobeyes.siriusinsight.io:3000'link='/weather' />
+  // <CameraStatus name="Camera 1" link='/cameras'/>,
+  // <CameraStatus name="Camera 2" link='/cameras'/>,
+  // <CameraStatus name="Camera 3" link='/cameras'/>,
+  // <CameraStatus name="Camera 4" link='/cameras' />,
+  <SystemBox name="Power" key="Power" endpoint='bobeyes.siriusinsight.io:3111' link='/power' />,
+  <SystemBox name="MRU" key="MRU" endpoint='bobeyes.siriusinsight.io:3004' link='/mru' />,
+  <SystemBox name="Weather" key="Weather" endpoint='bobeyes.siriusinsight.io:3000'link='/weather' />
 ];
 
 class Systems extends Component {
